aae110a280ca66fdb53281c0361351eb32d4d015
Brayyan Christopher Trujillo Valle <brayyanchristophertrujillovalle@macbook-air-de-soporteti.local>
2024-04-25 12:43:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/meditationapp/BottomMenuContent.kt
@@ -0,0 +1,9 @@
+package com.example.meditationapp
+
+import android.graphics.drawable.Icon
+import androidx.annotation.DrawableRes
+
+data class BottomMenuContent(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
